a515864509e718daa5cbf191fcbcac8e7deb64f8
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-07-27 20:20:38 +0530

    2 new projects added

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -13,7 +13,14 @@ function PortfolioPage() {
   const items = [
     {
       color: "from-blue-300 to-violet-300",
-      title: "FinanceMate",
+      title: "SyncStream",
+      desc: "FinanceMate is a financial management tool developed with the MERN stack. It helps users track budgets, expenses, and overall financial health through an intuitive dashboard featuring interactive charts and tables. Users can manage budgets, categorize expenses, and view detailed spending reports. With secure authentication and a user-friendly interface, FinanceMate simplifies financial planning and promotes better money management",
+      img: "/FinanceMate.png",
+      link: "https://financemate.vercel.app"
+    },
+    {
+      color: "from-blue-300 to-violet-300",
+      title: "InterviewIQ",
       desc: "FinanceMate is a financial management tool developed with the MERN stack. It helps users track budgets, expenses, and overall financial health through an intuitive dashboard featuring interactive charts and tables. Users can manage budgets, categorize expenses, and view detailed spending reports. With secure authentication and a user-friendly interface, FinanceMate simplifies financial planning and promotes better money management",
       img: "/FinanceMate.png",
       link: "https://financemate.vercel.app"
