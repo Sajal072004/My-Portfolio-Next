4b2df72ef77cc51afe90960f408e338e50ec689d
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-07-28 01:15:39 +0530

    change vw

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -96,7 +96,7 @@ function PortfolioPage() {
         <div className='sticky top-0 flex h-screen gap-4 items-center overflow-hidden'>
 
           <motion.div style={{ x }} className='flex'>
-            <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r from-violet-300 to-blue-300`}></div>
+            <div className={`h-screen w-[150vw] flex items-center justify-center bg-gradient-to-r from-violet-300 to-blue-300`}></div>
 
             {items.map((item, index) => {
               return (
